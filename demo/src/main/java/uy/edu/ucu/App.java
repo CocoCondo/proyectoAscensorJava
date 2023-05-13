b3bc6718be50ff92b52fe646aa1617ff89137107
CocoCondo <leonardo.conde@correo.ucu.edu.uy>
2023-05-13 19:17:29 -0300

    coso

---
+++ demo/src/main/java/uy/edu/ucu/App.java
@@ -0,0 +1,17 @@
+package uy.edu.ucu;
+
+/**
+ * Hello world!
+ */
+public final class App {
+    private App() {
+    }
+
+    /**
+     * Says hello to the world.
+     * @param args The arguments of the program.
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
